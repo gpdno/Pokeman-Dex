f0aad37027ab011976d9ea51b11a9e4674244274
gpdno <gdeninno@suncoastde.com>
2016-03-13 13:58:08 -0400

    updated with alamofire

---
+++ Pods/Target Support Files/Pods-Pokeman Dex/Pods-Pokeman Dex-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pokeman_Dex : NSObject
+@end
+@implementation PodsDummy_Pods_Pokeman_Dex
+@end
---
+++ Pods/Target Support Files/Pods-Pokeman Dex/Pods-Pokeman Dex-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Pokeman_DexVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Pokeman_DexVersionString[];
+
